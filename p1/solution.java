967ee2cb836c325b267fcb8897cc935d0f977b07
lantzmw <lantzmw@dukes.jmu.edu>
2016-12-12 21:16:01 -0500

    Problem 1
    Different ways to solve the problem 1 in different languages

---
+++ p1/solution.java
@@ -0,0 +1,22 @@
+public class Solution
+{
+    public int[] twoSum(int[] nums, int target) 
+    {
+    	int[] solution = new int[2];
+    	for(int i = 0; i < nums.length; i++)
+    	{	
+    		for(int j = i+1; j < nums.length; j++)
+    		{
+    			int sum = nums[i] + nums[j];
+    
+    			if(sum == target)
+    			{
+    				solution[0] = i;
+    				solution[1] = j;
+    			
+    			}
+    		}
+    	}
+    	return solution;
+    }
+}
